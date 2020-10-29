46fea4b4cca0b7c685ac9a3eb931c237ea98328f
T-A-R <t-a-r@mail.ru>
2020-10-29 16:27:13 +0300

    files

--- QUIZER_3/app/src/androidTest/java/com/example/quizer3/ExampleInstrumentedTest.java
+++ QUIZER_3/app/src/androidTest/java/com/example/quizer3/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.quizer.template", appContext.getPackageName());
+        assertEquals("pro.quizer.quizer3", appContext.getPackageName());
     }
 }

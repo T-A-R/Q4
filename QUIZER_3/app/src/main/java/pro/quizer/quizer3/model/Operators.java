d7e79127e72d10d7b0502ef432564b4153f771d0
T-A-R <t-a-r@mail.ru>
2020-12-02 17:50:58 +0300

    files

---
+++ QUIZER_3/app/src/main/java/pro/quizer/quizer3/model/Operators.java
@@ -0,0 +1,26 @@
+package pro.quizer.quizer3.model;
+
+import androidx.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+@StringDef({ElementType.BOX,
+        ElementType.QUESTION,
+        ElementType.TABLE,
+        ElementType.ROUTER,
+        ElementType.FUNNEL,
+        ElementType.CONTENT,
+        ElementType.ANSWER})
+public @interface ElementType {
+
+    String INFO = "info";
+    String QUESTION = "question";
+    String BOX = "box";
+    String TABLE = "table";
+    String ROUTER = "router";
+    String FUNNEL = "funnel";
+    String CONTENT = "content";
+    String ANSWER = "answer";
+}

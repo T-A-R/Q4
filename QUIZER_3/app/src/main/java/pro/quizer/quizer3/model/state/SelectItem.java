cb0b31976b59d986423b21bd136115d2a6d82877
T-A-R <t-a-r@mail.ru>
2021-01-25 18:15:27 +0300

    files

---
+++ QUIZER_3/app/src/main/java/pro/quizer/quizer3/model/state/SelectItem.java
@@ -0,0 +1,82 @@
+package pro.quizer.quizer3.model.state;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class AnswerState implements Serializable {
+
+    @SerializedName("relative_id")
+    private Integer relative_id;
+
+    @SerializedName("checked")
+    private boolean checked;
+
+    @SerializedName("open")
+    private boolean open;
+
+    @SerializedName("data")
+    private String data;
+
+    @SerializedName("enabled")
+    private boolean enabled;
+
+    public AnswerState() {
+        this.enabled = true;
+    }
+
+    public AnswerState(Integer relative_id, boolean checked, String data) {
+        this.relative_id = relative_id;
+        this.checked = checked;
+        this.data = data;
+        this.enabled = true;
+    }
+
+    public AnswerState(Integer relative_id, boolean checked, boolean open, String data, boolean enabled) {
+        this.relative_id = relative_id;
+        this.checked = checked;
+        this.open = open;
+        this.data = data;
+        this.enabled = enabled;
+    }
+
+    public Integer getRelative_id() {
+        return relative_id;
+    }
+
+    public void setRelative_id(Integer relative_id) {
+        this.relative_id = relative_id;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
